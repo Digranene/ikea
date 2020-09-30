c95e220f2b60621783bbdea6a3e5bfc4101ee33f
Digranene <Frozeninside21@gmail.com>
2020-09-30 23:22:44 +0300

    Changes day 2

--- script/generateFooter.js
+++ script/generateFooter.js
@@ -1,7 +1,7 @@
-'use strict'
+
 
 const generateFooter = () => {
-    const footer = `
+    const footerHTML = `
 <footer>
     <div class="container">
         <div class="footer">
@@ -40,6 +40,6 @@ const generateFooter = () => {
     </div>
 </footer>
     `
-    document.body.insertAdjacentHTML('beforebegin', footer)
+    document.body.insertAdjacentHTML('beforeend', footerHTML)
 }
-generateFooter()
+export default generateFooter

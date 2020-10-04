bbbd8a086eeef369932a509f3b74d9e6c925635d
Digranene <Frozeninside21@gmail.com>
2020-10-04 13:55:25 +0300

    Change cart

--- script/index.js
+++ script/index.js
@@ -1,16 +1,17 @@
 'use strict'
 
-import './Storage.js'
 import generateHeader from './generateHeader.js'
 import generateFooter from './generateFooter.js'
 import generateCatalog from './generateCatalog.js'
 import generateGoodsPage from "./generateGoodsPage.js";
+import generateCartPage from "./generateCartPage.js"
 import generateItemPage from "./generateItemPage.js";
-import { loadData } from './loadData.js'
+
 
 generateHeader()
 generateFooter()
 generateCatalog()
 generateGoodsPage()
+generateCartPage()
 generateItemPage()
-loadData()
+
